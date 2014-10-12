3c8f448d0e8b6ccdeaebdf67b69fd6b527f9cb68
skrivtsov <skrivtsov@provectus-it.com>
2014-10-12 13:58:57 +0300

    - project structure update

---
+++ db-manager-services/src/main/java/org/juke/webdb/spring/config/ServiceSpringConfig.java
@@ -0,0 +1,13 @@
+package org.juke.webdb.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Serhii Krivtsov
+ *
+ */
+@Configuration
+public class ServiceSpringConfig {
+
+}
